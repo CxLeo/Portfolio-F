83d62da04d295785862f47818324a7f869a4ad57
rohan <rohan@claclaws.com>
2024-09-04 10:50:12 -0400

    fixes

--- portfolio-f/components/globalcomponents/PageHeadingComponent.tsx
+++ portfolio-f/components/globalcomponents/PageHeadingComponent.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
-type props={
-    text:string,
-    subtext:string,
-    tag:string
+type props = {
+  text: string,
+  subtext: string,
+  tag: string
 }
 
-function PageHeadingComponent({text,subtext,tag}:props) {
+function PageHeadingComponent({ text, subtext, tag }: props) {
   return (
-    <div className='mt-[200px] mb-[20px] layout-2 flex 1130:items-center bg-purple-20 1130:flex-row flex-col gap-y-3' >
-      <h1 className='bg-red-40 text-[55px] md:text-[64px] flex-1 '>{text}</h1>
-      <h2 className='bg-yellow-5 text-[30px]  md:text-[32px] flex-1 text-[#898989]'>{subtext}</h2>
-    </div>
+    <>
+      <div className='mt-[200px] mb-[20px] layout-2 flex 1130:items-center bg-purple-20 1130:flex-row flex-col gap-y-3' >
+        <h1 className='bg-red-40 text-[55px] md:text-[64px] flex-1 flex flex-col '>
+          <span className='text-[16px] uppercase bg-[#00c7A8] w-fit ml-1 px-5 py-2 rounded-[27px]'>new</span>
+          {text}
+        </h1>
+        <h2 className='bg-yellow-5 text-[24px] sm:text-[30px]  md:text-[32px] flex-1 text-[#898989] 1130:mt-10'>{subtext}</h2>
+      </div>
+    </>
   )
 }
 
