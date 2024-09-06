99ce3cb27985e8a6b4a1df23948ffcf512083609
rohan <rohan@claclaws.com>
2024-09-06 11:52:32 -0400

    bug fixed for image animation for height

--- portfolio-f/components/globalcomponents/ImageHeaderComponent.tsx
+++ portfolio-f/components/globalcomponents/ImageHeaderComponent.tsx
@@ -28,19 +28,20 @@ function ImageHeaderComponent({image,height}:imageprops) {
         target: targetParallax,
         offset: ["end start", "start start"]
     })
-
+    const customheight=height?height:'1000px'
+    console.log(customheight)
     const y = useTransform(scrollYProgress, [1, 0], [0,250])
 
     return (
         < motion.div
-            className={`h-[50vh] md:h-[${!height?'1000px':height}]  overflow-y-clip`}>
+            className={`h-[50vh] md:h-[${customheight}]  overflow-y-clip`}>
 
             <motion.div
                 style={{ y }}
                 // style={{:scrollYProgress}}
                 ref={targetParallax}
                 className='   w-full h-full  '>
-                    <img className=' object-center object-cover md:object-cover  md:object-bottom w-full h-full' src={'/images/noq-banner.jpg'}/>
+                    <img className=' object-center object-cover md:object-cover  md:object-bottom w-full h-full' src={image}/>
                     {/* <img className='object-cover w-full h-full' src={'https://framerusercontent.com/images/EyzBztYZzL43QARvr8Hy7L2brY.png?scale-down-to=2048'}/> */}
                     {/* <Image width={10000} height={10000} alt='image-1' className='object-cover w-full h-full' src={'/images/noq-banner.jpg'}/> */}
 
